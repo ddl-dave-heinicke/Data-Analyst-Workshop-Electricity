23c15d43533de21d42071fbddb0220ad9ae96e3a
Data Analyst <akshay.ambekar+test.data.analyst.demo2@dominodatalab.com>
2022-09-28 16:50:10 -0700

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/test-data-analyst/PowerGenerationWorkshop?executionId=6324c359de659c33feb8f3df

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -118,68 +118,68 @@ def update_output(fuel_type, start_date):
                           x['datetime']+ datetime.timedelta(
                               minutes=30*(int(x['half_hour_increment'])-1))
                           , axis = 1)
-    # df_for_prophet = df[['datetime', fuel_type]].rename(columns = {'datetime':'ds', fuel_type:'y'})
-    # m = Prophet()
-    # m.fit(df_for_prophet)
-    # future = m.make_future_dataframe(periods=72, freq='H')
-    # fcst = m.predict(future)
-    # from https://github.com/facebook/prophet/blob/master/python/fbprophet/plot.py
+    df_for_prophet = df[['datetime', fuel_type]].rename(columns = {'datetime':'ds', fuel_type:'y'})
+    m = Prophet()
+    m.fit(df_for_prophet)
+    future = m.make_future_dataframe(periods=72, freq='H')
+    fcst = m.predict(future)
+    from https://github.com/facebook/prophet/blob/master/python/fbprophet/plot.py
     data = []
     # Add actual
-    # data.append(go.Scatter(
-    #     name='Actual',
-    #     x=m.history['ds'],
-    #     y=m.history['y'],
-    #     marker=dict(color=actual_color, size=marker_size),
-    #     mode='markers'
-    # ))
-    # # Add lower bound
-    # if uncertainty and m.uncertainty_samples:
-    #     data.append(go.Scatter(
-    #         x=fcst['ds'],
-    #         y=fcst['yhat_lower'],
-    #         mode='lines',
-    #         line=dict(width=0),
-    #         hoverinfo='skip'
-    #     ))
-    # # Add prediction
-    # data.append(go.Scatter(
-    #     name='Predicted',
-    #     x=fcst['ds'],
-    #     y=fcst['yhat'],
-    #     mode='lines',
-    #     line=dict(color=prediction_color, width=line_width),
-    #     fillcolor=error_color,
-    #     fill='tonexty' if uncertainty and m.uncertainty_samples else 'none'
-    # ))
-    # # Add upper bound
-    # if uncertainty and m.uncertainty_samples:
-    #     data.append(go.Scatter(
-    #         x=fcst['ds'],
-    #         y=fcst['yhat_upper'],
-    #         mode='lines',
-    #         line=dict(width=0),
-    #         fillcolor=error_color,
-    #         fill='tonexty',
-    #         hoverinfo='skip'
-    #     ))
-    # # Add caps
-    # if 'cap' in fcst and plot_cap:
-    #     data.append(go.Scatter(
-    #         name='Cap',
-    #         x=fcst['ds'],
-    #         y=fcst['cap'],
-    #         mode='lines',
-    #         line=dict(color=cap_color, dash='dash', width=line_width),
-    #     ))
-    # if m.logistic_floor and 'floor' in fcst and plot_cap:
-    #     data.append(go.Scatter(
-    #         name='Floor',
-    #         x=fcst['ds'],
-    #         y=fcst['floor'],
-    #         mode='lines',
-    #         line=dict(color=cap_color, dash='dash', width=line_width),
-    #     ))
+    data.append(go.Scatter(
+        name='Actual',
+        x=m.history['ds'],
+        y=m.history['y'],
+        marker=dict(color=actual_color, size=marker_size),
+        mode='markers'
+    ))
+    # Add lower bound
+    if uncertainty and m.uncertainty_samples:
+        data.append(go.Scatter(
+            x=fcst['ds'],
+            y=fcst['yhat_lower'],
+            mode='lines',
+            line=dict(width=0),
+            hoverinfo='skip'
+        ))
+    # Add prediction
+    data.append(go.Scatter(
+        name='Predicted',
+        x=fcst['ds'],
+        y=fcst['yhat'],
+        mode='lines',
+        line=dict(color=prediction_color, width=line_width),
+        fillcolor=error_color,
+        fill='tonexty' if uncertainty and m.uncertainty_samples else 'none'
+    ))
+    # Add upper bound
+    if uncertainty and m.uncertainty_samples:
+        data.append(go.Scatter(
+            x=fcst['ds'],
+            y=fcst['yhat_upper'],
+            mode='lines',
+            line=dict(width=0),
+            fillcolor=error_color,
+            fill='tonexty',
+            hoverinfo='skip'
+        ))
+    # Add caps
+    if 'cap' in fcst and plot_cap:
+        data.append(go.Scatter(
+            name='Cap',
+            x=fcst['ds'],
+            y=fcst['cap'],
+            mode='lines',
+            line=dict(color=cap_color, dash='dash', width=line_width),
+        ))
+    if m.logistic_floor and 'floor' in fcst and plot_cap:
+        data.append(go.Scatter(
+            name='Floor',
+            x=fcst['ds'],
+            y=fcst['floor'],
+            mode='lines',
+            line=dict(color=cap_color, dash='dash', width=line_width),
+        ))
     # Add trend
     if trend:
         data.append(go.Scatter(
@@ -190,18 +190,18 @@ def update_output(fuel_type, start_date):
             line=dict(color=trend_color, width=line_width),
         ))
     # Add changepoints
-    # if changepoints:
-    #     signif_changepoints = m.changepoints[
-    #         np.abs(np.nanmean(m.params['delta'], axis=0)) >= changepoints_threshold
-    #     ]
-    #     data.append(go.Scatter(
-    #         x=signif_changepoints,
-    #         y=fcst.loc[fcst['ds'].isin(signif_changepoints), 'trend'],
-    #         marker=dict(size=50, symbol='line-ns-open', color=trend_color,
-    #                     line=dict(width=line_width)),
-    #         mode='markers',
-    #         hoverinfo='skip'
-    #     ))
+    if changepoints:
+        signif_changepoints = m.changepoints[
+            np.abs(np.nanmean(m.params['delta'], axis=0)) >= changepoints_threshold
+        ]
+        data.append(go.Scatter(
+            x=signif_changepoints,
+            y=fcst.loc[fcst['ds'].isin(signif_changepoints), 'trend'],
+            marker=dict(size=50, symbol='line-ns-open', color=trend_color,
+                        line=dict(width=line_width)),
+            mode='markers',
+            hoverinfo='skip'
+        ))
  
     layout = dict(
         showlegend=False,
